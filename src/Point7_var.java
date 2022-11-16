6694b0b6f3ded924aa0b89bbed25b2141b3bf02e
Marie-Bocage <marie.bocage@gmail.com>
2022-11-16 10:21:36 +0100

    Modif point var

--- src/Point7_var.java
+++ src/Point7_var.java
@@ -9,7 +9,7 @@ public class Point7_var {
         // Nécessité d'initialiser la variable avec var ex :
         int nb; // Variable non initialisée mais qui peut être utilisée
 
-        var nombre; // variable non initialisée mais var ne comprend pas
+//        var nombre; // variable non initialisée mais var ne comprend pas
         var number=10; // variable initialisée avec var et java reconnait que c'est un interger
     }
 }
