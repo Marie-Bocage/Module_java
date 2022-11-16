0b61a57ccc0d1fc5ea5b93624016465244ac0beb
Marie-Bocage <marie.bocage@gmail.com>
2022-11-16 10:31:20 +0100

    Portee variable

---
+++ src/Point8_Portee.java
@@ -0,0 +1,9 @@
+public class Point8_Portee {
+    public static void main(String[] args) {
+        for (var i = 0; i <= 10; i++) {
+            System.out.println(i);
+        } // Porte de la variable correspond à l'ensemble des instructions ou elle est visible. Boucles ont accès à leur
+        // envi- ext mais les variables déclarées à l'int des boucles ne sont pas visibles à l'ext.
+//        System.out.println(i); // i n'a pas été déclaré en dehors de la boucle for
+    }
+}
