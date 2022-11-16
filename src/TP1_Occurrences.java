a91aee01e57b885f312717e0c07fa2937f4ffbcd
Marie-Bocage <marie.bocage@gmail.com>
2022-11-16 16:00:00 +0100

    TP Occurrence

---
+++ src/TP1_Occurrences.java
@@ -0,0 +1,28 @@
+import org.w3c.dom.ranges.DocumentRange;
+
+public class TP1_Occurrences {
+    public static void main(String[] args) {
+        int[] tab={2,1,5,6,7,7,6,2,1,7,1};
+
+        int maxOccurrence=0;
+        int elementFrequent=0;
+
+        for (int i = 0; i < tab.length; i++) {
+            int occurrence=0;
+
+            for (int j = 0; j < tab.length; j++) {
+                if (tab[i] == tab[j]) {
+                    occurrence++;
+                }
+                if (maxOccurrence < occurrence) {
+                    maxOccurrence=occurrence;
+                    elementFrequent=tab[i];
+                } else if (maxOccurrence == occurrence){
+                    maxOccurrence=maxOccurrence;
+                    elementFrequent=elementFrequent;
+                }
+            }
+        }
+        System.out.printf("Le nombre le plus fréquent est %d et sa fréquence d'apparition est de %d.", elementFrequent, maxOccurrence);
+    }
+}
