ae4551c5d2f3558afe4c8cf5139d83dfdab9aaee
Marie-Bocage <marie.bocage@gmail.com>
2022-11-16 13:37:56 +0100

    Point 10

---
+++ src/Point10_Foreach.java
@@ -0,0 +1,5 @@
+public class Point10_Foreach {
+    public static void main(String[] args) {
+        String[] programmingLanguage={"C", "C++", "Go", "Rust", "Java", "Typescript", "Python"};
+    }
+}
