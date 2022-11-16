65c78a2c49b4eb68631f093dad8513c957e13a2c
Marie-Bocage <marie.bocage@gmail.com>
2022-11-16 13:46:36 +0100

    Comparaison boucle for et foreach

--- src/Point10_Foreach.java
+++ src/Point10_Foreach.java
@@ -1,5 +1,14 @@
 public class Point10_Foreach {
     public static void main(String[] args) {
         String[] programmingLanguage={"C", "C++", "Go", "Rust", "Java", "Typescript", "Python"};
+
+//        for (int i = 0; i <= programmingLanguage.length - 1; i++) {
+//            System.out.println(programmingLanguage[i]);
+//        }
+
+        // Boucle for each -> Utile quand on a pas besoin de se reférer aux index ou de faire des op sur le tableau
+        for (String language:programmingLanguage) {
+            System.out.println(language);
+        }
     }
 }
