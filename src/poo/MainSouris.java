9e73c212cc85a18438281533337f3a5f3a36c899
Marie-Bocage <marie.bocage@gmail.com>
2022-11-18 16:58:36 +0100

    Exo Souris fini

--- src/poo/MainSouris.java
+++ src/poo/MainSouris.java
@@ -2,10 +2,16 @@ package poo;
 
 public class MainSouris {
     public static void main(String[] args) {
-        Souris testSouris=new Souris(5,"bleu",2,5);
+        Souris testSouris=new Souris(5,"grise");
         System.out.println(testSouris);
 
         Souris sourisCopie=new Souris(testSouris);
+        System.out.println(sourisCopie);
+
+        testSouris.evolue();
+        System.out.println(testSouris);
+
+        sourisCopie.evolue();
         System.out.println(sourisCopie);
     }
 }
--- src/poo/Souris.java
+++ src/poo/Souris.java
@@ -6,14 +6,18 @@ public class Souris {
     private int age;
     private String couleur;
     private int esperanceVie;
-    private String clonee;
+    private boolean clonee;
+
+    // Constructeur cosntante
+    public static final int ESPERANCE_VIE_DEFAUT=36;
 
     // Constructeurs
-    public Souris(int poids, String couleur, int age, int esperanceVie) {
+    public Souris(int poids, String couleur) {
         this.poids=poids;
         this.couleur=couleur;
         this.age=age;
-        this.esperanceVie=esperanceVie;
+        this.esperanceVie=ESPERANCE_VIE_DEFAUT;
+        this.clonee=false;
 
         System.out.println("Une nouvelle souris !");
     }
@@ -30,13 +34,77 @@ public class Souris {
         this.poids=souris.poids;
         this.couleur=souris.couleur;
         this.age=souris.age;
-        this.esperanceVie=souris.esperanceVie;
+        this.esperanceVie=(int)(0.8 * (double)souris.esperanceVie);
+        this.clonee=true;
 
-        System.out.println("Une nouvelle souris !");
+        System.out.println("Clonage d'une souris !");
     }
 
-    // Constructeur cosntante
-    public static final int ESPERANCE_VIE_DEFAUT=36;
+    // Getters
+    public int getPoids(){
+        return poids;
+    }
 
+    public String getCouleur(){
+        return couleur;
+    }
 
+    public int getAge(){
+        return age;
+    }
+
+    public int getEsperanceVie(){
+        return esperanceVie;
+    }
+
+    public boolean getClonee(){
+        return clonee;
+    }
+
+    // Setters
+    public void setPoids() {
+        if (poids > 0) {
+            this.poids=poids;
+        } else {
+            System.out.println("Le poids doit être supérieur à 0.");
+        }
+    }
+
+    public void setCouleur() {
+        this.couleur=couleur;
+    }
+
+    public void setAge() {
+            this.age=age;
+    }
+
+    public void setEsperanceVie() {
+            this.esperanceVie=esperanceVie;
+    }
+
+    public void setClonee() {
+        this.clonee=clonee;
+    }
+
+    // Méthode toString
+    public String toString() {
+        if (clonee == false) {
+            return String.format("Une souris %s, de %d mois et pesant %d grammes.", couleur, age, poids);
+        } else {
+            return String.format("Une souris %s, clonée, de %d mois et pesant %d grammes.", couleur, age, poids);
+        }
+    }
+
+    public void vieillir(){
+        age += 1;
+        if (clonee == true && age > esperanceVie/2) {
+            couleur="verte";
+        }
+    }
+
+    public void evolue() {
+        while (age < esperanceVie) {
+            this.vieillir();
+        }
+    }
 }
