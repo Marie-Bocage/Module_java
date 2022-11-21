b45c571a5cd242753e798f5fa07e18917845abbf
Marie-Bocage <marie.bocage@gmail.com>
2022-11-21 10:02:03 +0100

    Nouveau package  jeu+ ajout classesÃ

---
+++ src/jeux/Guerrier.java
@@ -0,0 +1,37 @@
+package jeux;
+
+public class Guerrier {
+    private String name;
+    private int dureeDeVie;
+    private String arme;
+
+    public Guerrier(String name, int dureeDeVie, String arme) {
+        this.name = name;
+        this.dureeDeVie = dureeDeVie;
+        this.arme = arme;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDureeDeVie() {
+        return dureeDeVie;
+    }
+
+    public void setDureeDeVie(int dureeDeVie) {
+        this.dureeDeVie = dureeDeVie;
+    }
+
+    public String getArme() {
+        return arme;
+    }
+
+    public void setArme(String arme) {
+        this.arme = arme;
+    }
+}
