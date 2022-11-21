329abd1d818098c492f36a9a96d13357212250a4
Marie-Bocage <marie.bocage@gmail.com>
2022-11-21 10:32:36 +0100

    Main jeux

---
+++ src/jeux/MainJeux.java
@@ -0,0 +1,9 @@
+package jeux;
+
+public class MainJeux {
+    public static void main(String[] args) {
+        var chris=new Guerrier("Christian Lisangola", 100,"Ak-47");
+        var marie=new Magicien("Marie Bocage", 150,"Marie Guette");
+        var abdelali=new Sorcier("Abdelali Akhyat", 200, "baguette Abdel", "baton Abdel");
+    }
+}
