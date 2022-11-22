5005e9af4b6bbda72553bc093cfcc9c0fbef9e99
Marie-Bocage <marie.bocage@gmail.com>
2022-11-22 14:42:51 +0100

    Corrections tp7

--- src/tpDeValidation/Tp7.java
+++ src/tpDeValidation/Tp7.java
@@ -4,16 +4,20 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Tp7 {
+    public static String alternance(String[] tab) {
+        for (int i = 0; i < tab.length; i++) {
+            if (i % 2 == 0) {
+                tab[i] = tab[i].toLowerCase();
+            } else {
+                tab[i] = tab[i].toUpperCase();
+            }
+        }
+        return Arrays.toString(tab);
+    }
     public static void main(String[] args) {
         String[] tab={"a","b","c","d","e"};
 
-        for (int i = 0; i < tab.length; i++) {
-            if (i % 2 == 0) {
-               tab[i] = tab[i].toLowerCase();
-            } else {
-               tab[i] = tab[i].toUpperCase();
-            }
-        }
-        System.out.println(Arrays.toString(tab));
+
+        System.out.println(alternance(tab));
     }
 }
